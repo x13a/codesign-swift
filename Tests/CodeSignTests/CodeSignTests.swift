bca9e8438bebede80e28cf99a65d6ee5a8790e36
lucky <>
2021-07-24 01:02:44 +0300

    add test case

--- Tests/CodeSignTests/CodeSignTests.swift
+++ Tests/CodeSignTests/CodeSignTests.swift
@@ -2,7 +2,20 @@
     @testable import CodeSign
 
     final class CodeSignTests: XCTestCase {
-        func testExample() throws {
-
+        func testApple() throws {
+            let code = try CodeSign.createCode(with: getppid()).get()
+            let requirement = try CodeSign.createRequirement(with: CodeSignRequirementString.apple).get()
+            try CodeSign.checkValidity(for: code, requirement: requirement).get()
+        }
+        
+        func testNotApple() throws {
+            let code = try CodeSign.createCode(with: getpid()).get()
+            let requirement = try CodeSign.createRequirement(with: CodeSignRequirementString.apple).get()
+            let result: Bool
+            switch CodeSign.checkValidity(for: code, requirement: requirement) {
+            case .success():  result = false
+            case .failure(_): result = true
+            }
+            assert(result)
         }
     }
